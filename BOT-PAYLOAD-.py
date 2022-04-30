7f8fd9cc61ad29443f80c43aff0e5bd0d8c7a381
scriptskali <104281088+scriptskali@users.noreply.github.com>
2022-04-29 21:05:44 -0400

    Create BOT-PAYLOAD-.py

---
+++ BOT-PAYLOAD-.py
@@ -0,0 +1,13 @@
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+#en desarrollo :(
